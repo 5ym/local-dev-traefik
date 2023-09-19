1f2888c29979a8056e75be2a9085d74f39f53c75
Ruk <16yuim@gmail.com>
2023-09-19 16:09:50 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/5ym/Local-Dev-Traefik.git docker
 cd docker
-cp docker-compose-sample.yml docker-compose.yml && cp traefik-sample.toml traefik.toml
+cp compose.sample.yml compose.yml && cp traefik-sample.toml traefik.toml
 cp traefik-sample.toml traefik.toml
 docker compose up -d
